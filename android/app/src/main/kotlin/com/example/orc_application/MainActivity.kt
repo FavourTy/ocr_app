e8187298e3a2e2b39b9654737a70109123db6d7f
FavourTy <you@example.com>
2023-10-12 07:09:39 -0700

    orc_app in progress

---
+++ android/app/src/main/kotlin/com/example/orc_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.orc_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+    theme: ThemeData(
+      useMaterial3: true
+    ),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
