3173b5b6fbfa3fd16d770d836d7a8fe5c8b1b08d
FavourTy <you@example.com>
2023-10-14 11:02:37 -0700

    reset page

---
+++ lib/Screens/reset.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ResetPasswordPage extends StatefulWidget {
+  const ResetPasswordPage({super.key});
+
+  @override
+  State<ResetPasswordPage> createState() => _ResetPasswordPageState();
+}
+
+class _ResetPasswordPageState extends State<ResetPasswordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
